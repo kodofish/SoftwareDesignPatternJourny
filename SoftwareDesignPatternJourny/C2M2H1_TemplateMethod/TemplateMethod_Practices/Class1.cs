cabc401796755201055668d85eb997e2a6e4afa6
kodofish <kodofish@gmail.com>
2023-01-10 01:13:27 +0800

    其實可以不需使用到 Func，透過 abstract class 就可以做到了

--- SoftwareDesignPatternJourny/C2M2H1_TemplateMethod/TemplateMethod_Practices/Class1.cs
+++ SoftwareDesignPatternJourny/C2M2H1_TemplateMethod/TemplateMethod_Practices/Class1.cs
@@ -4,24 +4,24 @@ namespace C2M2H1_TemplateMethod.TemplateMethod_Pratice
     {
         public void p1(int[] u)
         {
-            Process(u, (current, next) => Condition(current, next));
+            Process(u);
         }
-        private static bool Condition(int current, int next)
+        protected override bool Condition(int current, int next)
         {
             return current > next;
         }
     }
 
-    public class BaseClass
+    public abstract class BaseClass
     {
-        protected static void Process(int[] u, Func<int, int, bool> condition)
+        protected void Process(int[] u)
         {
             int n = u.Length;
             for (int i = 0; i < n - 1; i++)
             {
                 for (int j = 0; j < n - i - 1; j++)
                 {
-                    if (condition(u[j], u[j + 1]))
+                    if (Condition(u[j], u[j + 1]))
                     {
                         int temp = u[j];
                         u[j] = u[j + 1];
@@ -30,14 +30,15 @@ namespace C2M2H1_TemplateMethod.TemplateMethod_Pratice
                 }
             }
         }
+        protected abstract bool Condition(int current, int next);
     }
     public class Class2 : BaseClass
     {
         public void p2(int[] u)
         {
-            Process(u, (current, next) => Condition(current, next));
+            Process(u);
         }
-        private static bool Condition(int current, int next)
+        protected override bool Condition(int current, int next)
         {
             return current < next;
         }
