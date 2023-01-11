e925bb1fa86d93b841d32b416d59e72c98602892
kodofish <kodofish@gmail.com>
2023-01-12 01:07:55 +0800

    第一版 Template Method 練習

--- SoftwareDesignPatternJourny/C2M2H1_TemplateMethod/TemplateMethod_Practices/Level2_Practices.cs
+++ SoftwareDesignPatternJourny/C2M2H1_TemplateMethod/TemplateMethod_Practices/Level2_Practices.cs
@@ -1,43 +1,74 @@
 namespace C2M2H1_TemplateMethod.TemplateMethod_Pratice
 {
-    public class SearchLongestMessage {
-        public string search(string[] messages) {
-            var maxLengthMessage = "";
+    public abstract class TemplateClass<T>
+    {
+        protected T tempData = default(T);
+        protected T Search(string[] messages)
+        {
             foreach (var message in messages)
             {
-                if (message.Length > maxLengthMessage.Length) {
-                    maxLengthMessage = message;
-                }
-                Console.WriteLine(message);
-                
+                SearchLogic<T>(message);
             }
-            return maxLengthMessage;
+
+            return SearchResult();
+        }
+        protected abstract T SearchResult();
+
+        protected abstract void SearchLogic<T>(string message);
+        
+        protected static void OutputMessage(string message)
+        {
+            Console.WriteLine(message);
         }
     }
-    public class SearchEmptyMessageIndex {
-        public int search(String[] messages) {
-            var index = 0;
-            while (index < messages.Length && !string.IsNullOrEmpty( messages[index])) {
-                Console.WriteLine(messages[index]);
-                index ++;
+    public class SearchLongestMessage : TemplateClass<string>
+    {
+        protected override string SearchResult()
+        {
+            return tempData;
+        }
+        protected override void SearchLogic<T>(string message)
+        {
+            if (message.Length > tempData.Length)
+                tempData = message;
+            
+            OutputMessage(message);
+        }
+    }
+    public class SearchEmptyMessageIndex : TemplateClass<int>
+    {
+        private int emptyIndex = -1;
+        
+        protected override int SearchResult()
+        {
+            return emptyIndex;
+        }
+        protected override void SearchLogic<T>(string message)
+        {
+            if (string.IsNullOrEmpty(message))
+            {
+                if (emptyIndex == -1)
+                    emptyIndex = tempData;
             }
-            return index >= messages.Length ? -1 : index;
+            else
+                OutputMessage(message);
+
+            tempData++;
         }
     } 
     
-    public class CountNumberOfWaterballs {
-        public int count(string[] messages) {
-            var count = 0;
-            foreach (var message in messages)
-            {
-                if ("Waterball".Equals(message)) {
-                    count ++;
-                }
-                Console.WriteLine(message);
-                
+    public class CountNumberOfWaterballs : TemplateClass<int>
+    {
+        protected override int SearchResult()
+        {
+            return tempData;
+        }
+        protected override void SearchLogic<T>(string message)
+        {
+            if ("Waterball".Equals(message)) {
+                tempData ++;
             }
-            return count;
+            OutputMessage(message);
         }
     }
-    
 }
