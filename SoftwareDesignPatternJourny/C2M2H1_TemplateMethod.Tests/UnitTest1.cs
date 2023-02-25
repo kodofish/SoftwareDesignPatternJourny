12fd62f8a54da35298fd9e24d46e36611ea33350
kodofish <kodofish@gmail.com>
2023-02-25 22:48:53 +0800

    test: Implement unit test

---
+++ SoftwareDesignPatternJourny/C2M2H1_TemplateMethod.Tests/UnitTest1.cs
@@ -0,0 +1,17 @@
+namespace C2M2H1_TemplateMethod.Tests;
+
+using Showdown;
+public class RankTests
+{
+    [TestCase(Rank.Ace, Rank.Ace, 0)]
+    [TestCase(Rank.Ace, Rank.King, 1)]
+    [TestCase(Rank.Ace, Rank.Queen, 1)]
+    [TestCase(Rank.Ace, Rank.Jack, 1)]
+    [TestCase(Rank.Seven, Rank.Jack, -1)]
+    public void CompareTo(Rank rank1, Rank rank2, int expected)
+    {
+        var card1 = new PokerCard(Suit.Clubs, rank1);
+        var card2 = new PokerCard(Suit.Clubs, rank2);
+        Assert.AreEqual(expected, card1.CompareTo(card2));
+    }
+}
